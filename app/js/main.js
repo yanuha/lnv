305956ef168272d81f0fc0077148f3e803c324e4
Yanushko, Evgeny <yanuha2010@mail.ru>
2017-05-18 18:37:01 +0300

    added action for the contact card

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,48 @@
 function ready() {
+
+  // Class Handlers
+  var hasClass = function (elem, className) {
+      return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+  };
+
+  var addClass = function (elem, className) {
+      if (!hasClass(elem, className)) {
+          elem.className += ' ' + className;
+      }
+  };
+
+  var removeClass = function (elem, className) {
+      var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+      if (hasClass(elem, className)) {
+          while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+              newClass = newClass.replace(' ' + className + ' ', ' ');
+          }
+          elem.className = newClass.replace(/^\s+|\s+$/g, '');
+      }
+  };
+
+  // Show Card Functions
+  if ( 'querySelector' in document && 'addEventListener' in window ) {
+
+    var showCardItems = document.querySelectorAll('.js-lnv-contact-card-show-btn');
+    var hideCardBtn = document.querySelector('.js-lnv-contact-card-hide-btn');
+    var showCard = document.querySelector('.js-lnv-contact-card-w');
+
+    for (var i = 0, len = showCardItems.length; i < len; i++) {
+      var showCardItem = showCardItems[i];
+      showCardItem.addEventListener('click', function(e) {
+        addClass(showCard, 'js-show-card');
+      });
+
+      hideCardBtn.addEventListener('click', function(e) {
+        if ( hasClass(showCard, 'js-show-card') ) {
+            removeClass(showCard, 'js-show-card');
+        }
+      });
+    }
+  }
+
+  // sidebar show
   function classToggle() {
     document.querySelector('.js-layout-main-w').classList.toggle('active-sidebar');
   }
